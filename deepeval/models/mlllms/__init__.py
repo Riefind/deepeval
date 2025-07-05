4d18144c31391485f127c7080fdf17ffcda099df
Riefind <152933738+Riefind@users.noreply.github.com>
2025-07-05 18:48:33 +0700

    Handle optional model dependencies

--- deepeval/models/mlllms/__init__.py
+++ deepeval/models/mlllms/__init__.py
@@ -1,3 +1,16 @@
 from .openai_model import MultimodalOpenAIModel
-from .ollama_model import MultimodalOllamaModel
-from .gemini_model import MultimodalGeminiModel
+
+try:
+    from .ollama_model import MultimodalOllamaModel
+except Exception:  # pragma: no cover - optional dependency
+    MultimodalOllamaModel = None
+
+try:
+    from .gemini_model import MultimodalGeminiModel
+except Exception:  # pragma: no cover - optional dependency
+    MultimodalGeminiModel = None
+__all__ = ['MultimodalOpenAIModel']
+if MultimodalOllamaModel is not None:
+    __all__.append('MultimodalOllamaModel')
+if MultimodalGeminiModel is not None:
+    __all__.append('MultimodalGeminiModel')
